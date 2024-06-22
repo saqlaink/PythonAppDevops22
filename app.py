65c7d6f33c8522bd9a9c1feb724b4117f449f460
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-06-22 13:59:01 +0530

    updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'Hi Devops Geeks Welcome to the class123'
+        return 'Hi Devops Geeks Welcome to the class12345353453453'
 
     return app
 if __name__ == '__main__':

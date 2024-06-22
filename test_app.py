e4df19602f595d23a6af4518a7d0494ff849b00f
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-06-22 13:28:30 +0530

    updated test-app.py

--- test_app.py
+++ test_app.py
@@ -15,6 +15,6 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Hello World'
+    expected_text = 'Hi Devops Geeks Welcome to the class'
     print(response.data)
     assert expected_text.encode() == response.data
